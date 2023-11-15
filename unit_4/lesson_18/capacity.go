a4fa8fb73adc4c78249bddb5a4e837b23941d4a6
Julian Florez <forezjuli08@hotmail.com>
2023-11-15 09:53:42 -0500

    fix - formatting number to string

--- unit_4/lesson_18/capacity.go
+++ unit_4/lesson_18/capacity.go
@@ -5,7 +5,7 @@ import "fmt"
 func capacity(s []string, count int) []string {
 	for i := 0; i < count; i++ {
 		c := cap(s)
-		s = append(s, "New"+string(i))
+		s = append(s, "New_"+ fmt.Sprint(i))
 		if c != cap(s) {
 			fmt.Println("capacity changed!!!")
 			fmt.Printf("length: %v, capacity: %v\n", len(s), cap(s))
