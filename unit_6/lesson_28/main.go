f28d35484a5788a6fe893d5c9c85159a736b233b
Julian Florez <forezjuli08@hotmail.com>
2023-11-29 17:44:40 -0500

    add - first iteration of error handling

---
+++ unit_6/lesson_28/main.go
@@ -0,0 +1,51 @@
+// errors
+
+// lesson goals:
+// Write files and handle failure
+// Handle errors with a flair of creativity
+// Make and identify specific errors
+// Keep calm and carry on
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func proverbs(name string) error {
+	f, err := os.Create(name)
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintln(f, "Errors are values.")
+	if err != nil {
+		f.Close()
+		return err
+	}
+	_, err = fmt.Fprintln(f, "Don’t just check errors, handle them gracefully.")
+	f.Close()
+	return err
+
+}
+
+func main() {
+	files, err := os.ReadDir("../lesson_27")
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name(), "is Dir??", file.IsDir())
+	}
+
+	err2 := proverbs("proverbs.txt")
+
+  if err != nil {
+    fmt.Println(err2)
+    os.Exit(1)
+  }
+}
